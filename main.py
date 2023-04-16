500395a5398f0db7cc4183e4bcfb12e81c6978ac
Joaquin Lorca <joaquinlorca@uc.cl>
2023-04-16 14:04:51 -0400

    Modifiación dek main

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 nombre = input("Cual es tu nombre?: ")
 print("Hola", nombre)
+edad = int(input("¿Cuantos años tienes? "))
+if edad < 18:
+    print("No puedes votar")
+elif edad >= 18:
+    print("Puedes votar")
